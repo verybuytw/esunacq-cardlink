828456e6474c68aaab4be315d0dcb225a006abf9
Hughes <hughes@pht-studio.com>
2016-10-12 02:47:14 +0800

    modified

--- src/Request/AuthorizeRequest.php
+++ src/Request/AuthorizeRequest.php
@@ -4,6 +4,8 @@ namespace VeryBuy\Payment\EsunBank\Acq\CardLink\Request;
 
 class AuthorizeRequest extends RequestContract
 {
+    const TYPE_TRANSACTION = 'EC000001';
+    const TYPE_INSTALLMENT = 'EC000002';
     /**
      * @return array
      */
